17ce0df32442a8006334492ef3276231d0fd6e3d
Himanku Gogoi <himankugogoi5@gmail.com>
2022-12-07 23:18:47 +0530

    added actions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count+1);
+  };
+  const handleDecrement = () => {
+    setCount(count-1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count - {count}</h1>
+      <div>
+        <button onClick={handleDecrement}> DECREMENT </button>
+        <button onClick={handleIncrement}> INCREMENT </button>
+      </div>
     </div>
   );
 }
